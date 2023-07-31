ca665a7df6bb4d4df264a063a8471c92932ae7ac
zero1001god <141078585+zero1001god@users.noreply.github.com>
2023-07-31 22:57:16 +0000

    ejercicio

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 #mi primer main en phyton xd
+#ejercicio 1
+print ("hola mundo")
+
+
+mensaje = "hola mundooo"
+print (mensaje)
+
+NUMERO1 = int(input("ingresa el primer numero "))
+NUMERO2 = int(input("ingresa el otro numero "))
+
+print ("la suma es " + str (NUMERO1 + NUMERO2))
+
+
+   
+
+
+
+    
