b6e0591a38b644e2cdd718ec6208602b754d3cff
zero1001god <141078585+zero1001god@users.noreply.github.com>
2023-08-02 21:25:01 +0000

    ejercicip

--- main.py
+++ main.py
@@ -6,12 +6,9 @@ print ("hola mundo")
 mensaje = "hola mundooo"
 print (mensaje)
 
-NUMERO1 = int(input("ingresa el primer numero "))
-NUMERO2 = int(input("ingresa el otro numero "))
 
-print ("la suma es " + str (NUMERO1 + NUMERO2))
-
-
+NOMBRE = input("ingresa tu nombre ")
+print ("hola " + NOMBRE + " MUCHO GUSTO")
    
 
 
