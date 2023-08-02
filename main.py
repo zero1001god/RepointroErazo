c6a78e22cb3aa00b54afed538d0507a8cd18be65
zero1001god <141078585+zero1001god@users.noreply.github.com>
2023-08-02 23:34:58 +0000

    ps

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
 #mi primer main en phyton xd
 #ejercicio 1
-print ("hola mundo")
+#print ("hola mundo")
+
+#ejercicio 2
+#mensaje = "hola mundooo"
+#print (mensaje)
+
+#ejercicio 3
+#NOMBRE = input("ingresa tu nombre ")
+#print ("hola " + NOMBRE + " MUCHO GUSTO")
+
+#Numero = int(input("hola " + NOMBRE + " ingresa tu edad"))
+#if Numero >= 18:
+  #print("felicidades eres mayor de edad")
+#if Numero < 18:
+  #print("bro sos joven")
+#if Numero < 1:
+  #print ("no has nacido no seas pendejo")
+#if Numero > 110:
+  #print ("milagro que sigues vivo")
+
+#ejercicio 4
+#ejercicio = ((3+2)/(2*5)) ** 2
+#print (ejercicio)
+
+#ejercicio 5
+#Horas = float(input("numero de horas diarias "))
+#Costo = float(input("ahora cuanto te pagan por hora? "))
+#operacion = str(Horas * Costo)
+#print ("tu pago total en el día es " + operacion + " pesos")
+''''''
+print ("dame un entero positivo")
+N = int(input())
+print ("EL RESULTADO ES" + str(N*(N+1)//2) )
+'''
+#N = int(input("dame un entero"))
+#suma = str(N*(N+1)//2)
+#print ("el resultado es" + suma)
+''''''
+#ejercicio 7
+print("cuanto pesas?")
+peso = (input())
+print("cuanto mides")
+estatura =(input())
+imc = round(float(peso)/float(estatura)**2,2)
+print ("tu imc es" , imc)'''
+
+#ejercicio 8
+print("dame un numero entero")
+n=int(input())
+print("dame otro bro")
+m=int(input())
+print("el cociente es")
+cociente = (n/m)
+print(cociente)
+print ("el residuo es")
+residuo = (n%m)
+print (residuo)
 
 
-mensaje = "hola mundooo"
-print (mensaje)
-
-
-NOMBRE = input("ingresa tu nombre ")
-print ("hola " + NOMBRE + " MUCHO GUSTO")
-   
-
-
-
-    
+#ejercicio 
